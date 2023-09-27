88e730e31a4f2a45403bba95dbd9244d63e56f18
james <ueberjames@gmail.com>
2023-09-27 09:38:47 -0300

    Add project files.

---
+++ EnqueteWeb/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using EnqueteWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EnqueteWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult Responder()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Responder(Resposta form)
+        {
+            return Content("Recebemos sua resposta.");
+        }
+
+
+    }
+}
---
+++ EnqueteWeb/Models/Resposta.cs
@@ -0,0 +1,9 @@
+﻿namespace EnqueteWeb.Models
+{
+    public class Resposta
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public bool? Sim { get; set; }
+    }
+}
